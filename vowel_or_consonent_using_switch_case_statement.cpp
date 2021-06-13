4f44c07d3a5717f745a45ceeabb75e0ccc8ac095
Arnab Sarkar <84896269+asarkar6589@users.noreply.github.com>
2021-06-13 12:26:33 +0530

    Add files via upload

---
+++ vowel_or_consonent_using_switch_case_statement.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    cout << "Character: ";
+    cin >> c;
+
+    switch (c)
+    {
+    case 'a':
+        cout << "vowel" << endl;
+        break;
+
+    case 'e':
+        cout << "vowel" << endl;
+        break;
+
+    case 'i':
+        cout << "vowel" << endl;
+        break;
+
+    case 'o':
+        cout << "vowel" << endl;
+        break;
+
+    case 'u':
+        cout << "vowel" << endl;
+        break;
+
+    case 'A':
+        cout << "vowel" << endl;
+        break;
+
+    case 'E':
+        cout << "vowel" << endl;
+        break;
+
+    case 'I':
+        cout << "vowel" << endl;
+        break;
+
+    case 'O':
+        cout << "vowel" << endl;
+        break;
+
+    case 'U':
+        cout << "vowel" << endl;
+        break;
+
+    default:
+        cout << "Consonent" << endl;
+        break;
+    }
+
+    return 0;
+}
